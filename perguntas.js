07857fcc8269c1e733dd18227ab74ef788f110dc
Deathzinho <00001100953206sp@al.educacao.sp.gov.br>
2024-11-06 14:09:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Avião',
+    'Quem foi o pai da aviação?',
+    'Santos Dumont'
+)
+
+criaCartao(
+    'Militar',
+    'O que significa AFA?',
+    'Academia da Força Aérea'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual foi o jodador que fez mais gols do Corinthians?',
+    'Claudio(305 gols)'
+)
+
+criaCartao(
+    'Futebol',
+    'Em que ano o Pelé nasceu?',
+    '1940'
+)
